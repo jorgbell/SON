a33af89c8e42735f5e31eeb1e13c2cb2866c4cc3
jorgbell <jorgbell@ucm.es>
2021-12-08 19:20:05 +0100

    Acabado el escenario, lo que falta son los eventos aleatorios listados en el script

---
+++ PROJECT DISCO/Assets/Src/Misc/RandomEvents.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomEvents : MonoBehaviour
+{
+    //Script separado a GameManager por comodidad, gestionará los eventos aleatorios de sonido que podrán ocurrir
+
+    //LISTA DE EVENTOS ALEATORIOS:
+    //Vasos de cristal rompiendose ->fmod
+    //Gente tirandose a la piscina ->FMOD
+    //Cambio de estado de los NPC de las diferentes salas
+    //Scratch de la musica y vuelta a empezar
+    //Tirar del vater y abrir el grifo del baño
+    private void Update()
+    {
+        
+    }
+}
